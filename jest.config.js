32824dabb283da647a1dc35c4123479fb39787a0
Matheus Rocha <matheus_rocha98@outlook.com>
2020-12-04 12:20:52 -0300

    build: setup eslint, prettier, jest & testing-library

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)',
+    '!src/**/*.stories.ts(x)'
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/.jest/setup.ts'
+  ]
+}
---
+++ src/components/Main/main.component.tsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+  return (
+    <main>
+      <h1>NextJS is awesome!</h1>
+    </main>
+  )
+}
