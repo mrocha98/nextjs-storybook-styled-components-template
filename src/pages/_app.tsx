be13006e3faa3237e41922c53362f3cb183c3d0a
Matheus Rocha <matheus_rocha98@outlook.com>
2020-12-04 13:07:04 -0300

    feat: setup styled-components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { GlobalStyles } from 'styles/global'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>NextJS Storybook template</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple projet to work with TypeScript, React, NextJS, Styled Components and Storybook!"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
