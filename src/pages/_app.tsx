5e67fb85033bb889f215bb22131dea47140e0c72
Matheus Rocha <matheus_rocha98@outlook.com>
2020-12-05 19:57:12 -0300

    feat: setup pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>NextJS Storybook template</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple projet to work with TypeScript, React, NextJS, Styled Components and Storybook!"
